742bd580d1a8e03a50a4255995c7a7d3b70e07e5
Andres Pena <penchjpc@gmail.com>
2022-02-14 17:53:20 -0500

    successfully added code for moving pacman in any direction

--- index.js
+++ index.js
@@ -43,41 +43,66 @@ const layout = [
 ]
 
 const squares = []
-
+console.log(grid)
 function createBoard(){
-    for (const element of layout){
+    for (let i=0; i<layout.length; i++){
         // create square
-        const square = document.createElement('div')
-        // give square a class
-        if(element===0){
-            square.classList.add('pac-dot')
-        }else if(element===1){
-            square.classList.add('wall')
-        }else if(element===2){
-            square.classList.add('ghost-lair')
-        }else if (element===3){
-            square.classList.add('power-pellet')
-        }else{
-            square.classList.add('empty')
-        }
+        const square = document.createElement('div')        
         // add square to grid
         grid.appendChild(square)
         // save square in squares array
         squares.push(square)
-        // if(element===0){
-        //     squares[i].classList.add('pac-dot')
-        // }else if(element===1){
-        //     squares[i].classList.add('wall')
-        // }else if(element===2){
-        //     squares[i].classList.add('ghost-lair')
-        // }else if (element===3){
-        //     squares[i].classList.add('power-pellet')
-        // }else{
-        //     squares[i].classList.add('empty')
-        // }       
+        // give square a class this works because arrays work as references
+        // for objects. Html elements, in this case 'divs' are objects.
+        if(layout[i]===0){
+            squares[i].classList.add('pac-dot')
+        }else if(layout[i]===1){
+            squares[i].classList.add('wall')
+        }else if(layout[i]===2){
+            squares[i].classList.add('ghost-lair')
+        }else if (layout[i]===3){
+            squares[i].classList.add('power-pellet')
+        }else{
+            squares[i].classList.add('empty')
+        }       
 
     }
 }
-
+ 
 createBoard()
 
+// Index of pacman's position
+let currentIndex = 500
+squares[currentIndex].classList.add('pacman')
+
+// Control the movements of pacman
+function control(e){
+    // erase pacman from starting position
+    squares[currentIndex].classList.remove('pacman')
+    // left
+    if(e.key === 'ArrowLeft'){        
+        currentIndex -=1
+        // draw pacman in new position
+        squares[currentIndex].classList.add('pacman')
+    }else if (e.key ==='ArrowUp'){
+    // up    
+        currentIndex -=28
+        // draw pacman in new position
+        squares[currentIndex].classList.add('pacman')
+
+    }else if (e.key==='ArrowRight'){
+    // right
+        currentIndex +=1
+        // draw pacman in new position
+        squares[currentIndex].classList.add('pacman')
+    }else if (e.key ==='ArrowDown'){
+    // down    
+        currentIndex +=28
+        // draw pacman in new position
+        squares[currentIndex].classList.add('pacman')
+    }
+}
+
+
+// event listener for arrow keys in the document
+document.addEventListener('keydown', control)
